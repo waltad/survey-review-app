9b68c77faf7a6872985939d292b314061e99b8cc
Tadek <tadek.waluga@gmail.com>
2025-05-28 21:09:08 +0200

    Adding first layout

--- app.py
+++ app.py
@@ -13,6 +13,15 @@ st.write("Przegląd danych z ankiety powitalnej z kursu `Od zera do AI`.")
 
 df = pd.read_csv('35__welcome_survey_cleaned.csv', sep=';')
 
+c0, c1, c2 = st.columns(3)  # Create three columns for layout
+
+with c0:
+    st.metric(label="Liczba uczestników", value=len(df))  # Display the number of unique participants
+with c1:
+    st.metric(label="Liczba kobiet", value=len(df[df['gender']==1]))  # Display the number of women
+with c2:
+    st.metric(label="Liczba mężczyzn", value=len(df[df['gender']==0]))  # Display the number
+
 st.subheader("Przykładowe dane z ankiety") # Set a subheader for the data section
 st.dataframe(df.head(5), hide_index=True)  # Display the first 5 rows of the DataFrame
 
